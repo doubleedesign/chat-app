5dbc0a2f0a0b88db1639cf3ec504931fdfe850fc
Leesa Ward <leesa@doubleedesign.com.au>
2024-09-10 13:28:02 +1000

    Set up a basic endpoint and Swagger docs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,11 @@
 import express from 'express';
+import expressJSDocSwagger from 'express-jsdoc-swagger';
+import users from './data/users.json' assert { type: 'json' };
+import groups from './data/groups.json' assert { type: 'json' };
+import { fileURLToPath } from 'url';
+import path from 'node:path';
+
+// Basic setup
 const app = express();
 const port = process.env.PORT || 4100;
 
@@ -9,6 +16,60 @@ app.get('/', (req, res) => {
 	res.json({
 		message: 'Hello Chatty!'
 	});
+});
+
+/**
+ * GET /groups
+ * @summary Get a given user's groups
+ * @param {string} userId.query.required - The user's email
+ * @return {object} 200 - success response
+ */
+app.get('/groups', (req, res) => {
+	const userId = req.query.userId;
+
+	// If no userId, return 400
+	if (!userId) {
+		return res.status(400).json({
+			error: 'userId is required'
+		});
+	}
+
+	// Find the user
+	const user = users.find(user => user.email === userId);
+
+	// If user not found, return 404
+	if (!user) {
+		return res.status(404).json({
+			error: 'User not found'
+		});
+	}
+
+	return res.status(200).json(user.groupIds.map(groupId => {
+		return groups.find(group => group.id === groupId);
+	}));
+});
+
+// Documentation setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+expressJSDocSwagger(app)({
+	info: {
+		version: '0.0.1',
+		title: 'Chatty',
+	},
+	security: {
+		BasicAuth: {
+			type: 'http',
+			scheme: 'basic',
+		},
+	},
+	baseDir: __dirname,
+	filesPattern: './**/*.ts',
+	swaggerUIPath: '/docs',
+	exposeSwaggerUI: true,
+	exposeApiDocs: false,
+	notRequiredAsNullable: false,
+	swaggerUiOptions: {},
 });
 
 app.listen(port, () => {
--- server/src/types.ts
+++ server/src/types.ts
@@ -6,6 +6,13 @@ export enum UserRoles {
 
 export type UserId = string;
 
+/**
+ * @typedef {object} User
+ * @property {string} email.required - User's email address
+ * @property {string} name.required - User's full name
+ * @property {string} avatar - URL to the user's avatar
+ * @property {string[]} groupIds - Array of group IDs the user is a member of
+ */
 export type User = {
 	email: UserId;
 	name: string;
@@ -19,9 +26,23 @@ type Container = {
 	avatar?: string;
 };
 
+/**
+ * @typedef {object} Group
+ * @property {string} id.required - Group ID
+ * @property {string} label.required - Group name
+ * @property {string} avatar - URL to the group's avatar
+ * @property {string[]} admins - Array of user emails that correspond to admin user accounts
+ * @property {Channel[]} channels - Array of channels in the group
+ */
 export type Group = Container & {
 	channels: Container[];
 	admins: UserId[];
 };
 
+/**
+ * @typedef {object} Channel
+ * @property {string} id.required - Channel ID
+ * @property {string} label.required - Channel name
+ * @property {string} avatar - URL to the channel's avatar
+ */
 export type Channel = Container;
